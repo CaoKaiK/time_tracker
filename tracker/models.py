d7a876bfc8db820aa1ffc758ae0a9e5b7abbca54
Niklas <niklas.kueppers@rub.de>
2019-11-15 12:57:48 +0100

    EXTEND install pillow

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class entries(models.Model):
+    date = models.DateField('Date')
+    start = models.DateTimeField('Start Time')
+    end = models.DateTimeField('End Time')
+    duration = models.ImageField()
+
