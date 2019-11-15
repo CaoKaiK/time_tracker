c3dd4597ecc7f998a472d5378a2eeeda6dc48da9
Niklas <niklas.kueppers@rub.de>
2019-11-15 11:59:53 +0100

    EXTEND defined database and app

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'timetracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DB_DIR = 'database'
+
+#DATABASE_ROUTERS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': os.path.join(BASE_DIR, DB_DIR, 'db.sqlite3'),
+    },
 }
 
 
