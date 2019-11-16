34d66b1ae52fd8d1a6a0b44be7a85b99148ecd13
Niklas <niklas.kueppers@rub.de>
2019-11-16 14:38:38 +0100

    EXTEND fix database and start on tests

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Entry
+from .models import Project, Element, Entry, Account, Day
 
+admin.site.register(Project)
+admin.site.register(Element)
 admin.site.register(Entry)
 
+admin.site.register(Account)
+admin.site.register(Day)
