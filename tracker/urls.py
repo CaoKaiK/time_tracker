3508651f72e2c8c7bb4c49bd92f0d7afd88d2b2f
Niklas <niklas.kueppers@rub.de>
2019-11-17 20:03:51 +0100

    EXTEND projects deleteview

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,13 +4,17 @@ from .views import (
     ProjectListView,
     ProjectDetailView,
     ProjectCreateView,
+    ProjectUpdateView,
+    ProjectDeleteView,
     EntryListView)
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('projects/', ProjectListView.as_view(), name='projects'),
+    path('projects/', ProjectListView.as_view(), name='projects-list'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='projects-detail'),
     path('projects/create/', ProjectCreateView.as_view(), name="projects-create"),
+    path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='projects-update'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name="projects-delete"),
     path('entries/', EntryListView.as_view(), name='entries'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from django.contrib.messages.views import SuccessMessageMixin
@@ -24,11 +24,24 @@ class ProjectCreateView(SuccessMessageMixin, CreateView):
         'project_name',
         'country_name',
         ]
-
     success_message = "Project %(project_name)s was created"
-
     def get_success_url(self):
         return reverse('projects-detail', kwargs={'pk': self.object.pk})
+
+class ProjectUpdateView(SuccessMessageMixin, UpdateView):
+    model = Project
+    fields = [
+        'project_name',
+        'country_name',
+    ]
+    success_message = "Project %(project_name)s was updated"
+    def get_success_url(self):
+        return reverse('projects-detail', kwargs={'pk': self.object.pk})
+
+class ProjectDeleteView(SuccessMessageMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy('projects-list')
+
 
 class EntryListView(ListView):
     model = Entry
