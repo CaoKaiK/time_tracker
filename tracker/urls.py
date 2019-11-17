6754cc3778ca20a00145641802643a6b2bbb765b
Niklas <niklas.kueppers@rub.de>
2019-11-17 19:18:15 +0100

    EXTEND create project

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from .views import (
-    ProjectListView, 
-    ProjectDetailView, 
+    ProjectListView,
+    ProjectDetailView,
+    ProjectCreateView,
     EntryListView)
 from . import views
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', ProjectListView.as_view(), name='projects'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='projects-detail'),
+    path('projects/create/', ProjectCreateView.as_view(), name="projects-create"),
     path('entries/', EntryListView.as_view(), name='entries'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Project, Entry
 
@@ -10,10 +13,18 @@ def home(request):
 class ProjectListView(ListView):
     model = Project
     context_object_name = 'projects'
-    paginate_by = 10
+    paginate_by = 20
 
 class ProjectDetailView(DetailView):
     model = Project
+
+class ProjectCreateView(CreateView, SuccessMessageMixin):
+    model = Project
+    fields = ['project_name']
+    success_message = "Project %(name)s was created" #TO DO
+
+    def get_success_url(self):
+        return reverse('projects-detail', kwargs={'pk': self.object.pk})
 
 class EntryListView(ListView):
     model = Entry
