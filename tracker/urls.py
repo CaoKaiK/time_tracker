db4dd18995d4f9a62409185d5635eed02fd3451a
Niklas <niklas.kueppers@rub.de>
2019-11-21 22:22:29 +0100

    IMPLEMENT element detail view + entries in view

--- tracker/urls.py
+++ tracker/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='projects-update'),
     path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name="projects-delete"),
     path('projects/<int:pk>/element/create', ElementCreateView.as_view(), name='projects-element-create'),
-    path('projects/<int:pk>/element/<int:pk_entry>', ElementDetailView.as_view(), name='projects-element-detail'),
+    path('projects/<int:pk_pro>/element/<int:pk>', ElementDetailView.as_view(), name='projects-element-detail'),
     path('entries/', EntryListView.as_view(), name='entries'),
 
 ]
