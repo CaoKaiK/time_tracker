4e56dd803040c30c8cbb42b2d290d946f901d730
Niklas <niklas.kueppers@rub.de>
2019-12-17 22:32:58 +0100

    ?

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -20,14 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'quba%h$jbpbk%62+x585qgvxy&o3$q7mn-%g@lvo-h4+@+ei71'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'quba%h$jbpbk%62+x585qgvxy&o3$q7mn-%g@lvo-h4+@+ei71'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'siemenstimetracker.herokuapp.com'
+    'siemenstimetracker.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -132,10 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
